24b130f6df8a54d58ea4e62b1ea66c9058e38c3f
Thiago Tavares <thiago.tlopess@gmail.com>
2024-06-12 17:05:38 -0300

    att

--- ugo-func.R
+++ ugo-func.R
@@ -1,33 +1,34 @@
 # PROBABILITY DENSITY FUNCTION
-dUGo <- function(x, alpha = 0.5, theta = 2, log = FALSE)
+
+dUGo <- function(x, mu=.5, sigma=1.2, tau=.5)
 {
-  if (any(alpha <=0) | any(alpha >=1)) stop(paste("alpha must be between 0 and 1"))
-  if (any(theta<=0)) stop(paste("theta must be posite"))
-  if (any(x <= 0) | any(x >= 1)) stop(paste("x must be between 0 and 1"))
-  fx1 <- alpha*theta*x^(-1-theta)*exp(alpha*(1-x^-theta)) 
-  if(log==FALSE) fx<-fx1 else fX<-log(fx1)
+  
+  fx<- log(tau)/(1 - mu^(-sigma)) * sigma * x^(-(1 + sigma)) * exp((log(tau) / (1 - mu^(-sigma))) *
+                                                                      (1 - x^(-sigma)))
   return(fx)
+  
 }
 
-#CUMULATIVE DISTRIBUTION FUNCTION
-pUGo<- function(q, alpha = 0.5, theta = 2, lower.tail = TRUE, log.p = FALSE)
+integrate(dUGo,0,1) 
+
+# CUMULATIVE DISTRIBUTION FUNCTION 
+
+pUGo <-  function(q, mu=.5, sigma=1.2, tau=.5) 
 {
-  if (any(alpha <= 0) | any(alpha >= 1)) stop(paste("alpha must be between 0 and 1"))
-  if (any(theta < 0)) stop(paste("theta must be positive"))
-  if (any(q <= 0) | any(q >= 1)) stop(paste("q must be between 0 and 1")) 
-  cdf1 <- exp(alpha*(1-q^(-theta)))
-  if(lower.tail==TRUE) cdf<-cdf1 else cdf<- 1-cdf1
-  if(log.p==FALSE) cdf<- cdf else cdf<- log(cdf)
+  cdf <- exp(((log(tau)) / (1 - mu^(-sigma))) * (1 - q^(-sigma)))
   return(cdf)
 }
 
+pUGo(.25)
+integrate(dUGo, 0, .25)
 
-# QUANTILE FUNCTION 
-qUGo<-function(u, alpha ,theta)
+# QUANTILE FUNCTION
+qUGo<-function(u, mu=.5, sigma=1.2, tau=.5)
 {
-  q<- (1- (log(u/alpha)))^(-1/theta)
+  q<-(1 - (log(u) * (1 - mu^(-sigma)) / log(tau)))^(-1 / sigma)
   return(q)
 }
 
-
+#u=pUGo(.2)
+#qUGo(u,mu=.5, sigma=1.2) 
 
