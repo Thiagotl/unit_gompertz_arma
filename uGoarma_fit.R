772291143bb4538cee2bd8d29e16a4c69184dfe9
Thiago Tavares <thiago.tlopess@gmail.com>
2024-06-17 17:31:46 -0300

    Att

--- uGoarma_fit.R
+++ uGoarma_fit.R
@@ -1,7 +1,33 @@
 
-uGoarma.fit<-function(x, ar = NA, ma = NA, tau = .5, link = "logit", h = 0, 
+uGoarma.fit<-function(y, ar = NA, ma = NA, tau = .5, link = "logit", h = 0, 
                       diag = 0, X = NA, X_hat = NA)
 {
   source("ugo-functions.r")
+  # adicionar o teste lógico para y
+  # adicionar o teste lógico para a série temporal
   
+  z<-c()
+  max_it1<-50
+  p <- max(ar)
+  q <- max(ma)
+  n <- length(x)
+  m <- max(p, q, na.rm = TRUE) 
+  y1 <- y[(m+1):n]
+  p1 <- length(ar)
+  q1 <- length(ma)
+  error <- rep(0,n)
+  eta <- rep(NA,n)
+  
+  y_prev <- c(rep(NA, (n+h)))
+  
+  linktemp <- substitute(link)
+  
+  if(!is.character(linktemp))
+  {
+    linktemp<-deparse
+  }
+  
+  link = linktemp
+
+
 }
