100ca157c124d7baa001fedbb428e82ae68cedb3
Thiago Tavares <thiago.tlopess@gmail.com>
2024-07-05 19:35:02 -0300

    att

--- monte_carlo.R
+++ monte_carlo.R
@@ -3,8 +3,8 @@
 
 phi=0.2;theta=0.4; alpha=1;sigma=6; tau=0.5
 true_values=c(1,0.2,0.4,6)
-n=100
-R<-100000
+n=50
+R<-100
 mu_result<-matrix(NA,R,4)
 for (i in 1:R) {
 
@@ -21,7 +21,9 @@ eqm_values<-c(apply(mu_result,2,var))+(true_values-mean_values)^2
 result1<- cbind(true_values,
                 mean_values,
                 b_values,
-                eqm_values)
+                eqm_values
+)
 colnames(result1)<-c("true value","mean","bias","eqm")
-rownames(result1)<-c("b1","b2","g1","g2")
+rownames(result1)<-c("alpha","phi","theta","sigma") 
 print(round(result1,5))
+
