cd7965ed58ced259f701d4acc4acc57813bf6ecc
Thiago Tavares <thiago.tlopess@gmail.com>
2024-07-14 22:31:23 -0300

    att

--- monte_carlo.R
+++ monte_carlo.R
@@ -1,82 +1,113 @@
-# # Checking the results
-# Case 2: with regressors
-
-phi=0.2;theta=0.4; alpha=1;sigma=6; tau=0.5
-true_values=c(1,0.2,0.4,6)
-n=50
-R<-100
-mu_result<-matrix(NA,R,4)
-for (i in 1:R) {
-
-  y<-simu.ugoarma(n,phi=phi,theta=theta, alpha=alpha,sigma=sigma, tau=tau,freq=12,
-                    link="logit")
-  fit1<-uGoarma.fit(y)
-  mu_result[i,]<-fit1$coeff
-}
-
-
-mean_values<-c(apply(mu_result,2,mean))
-b_values<-(true_values-mean_values)/true_values*100
-eqm_values<-c(apply(mu_result,2,var))+(true_values-mean_values)^2
-result1<- cbind(true_values,
-                mean_values,
-                b_values,
-                eqm_values
-)
-colnames(result1)<-c("true value","mean","bias","eqm")
-rownames(result1)<-c("alpha","phi","theta","sigma") 
-print(round(result1,5))
+# # # Checking the results
+# # Case 2: with regressors
+# 
+# #initial parametres
+# 
+# phi=0.2 # AR
+# theta=0.4 #MA
+# alpha=1
+# sigma=6 
+# tau=0.5
+# true_values=c(1,0.2,0.4,6)
+# 
+# #sample size
+# n=c(70, 150, 300, 500)
+# z=1.96
+# R=100
+# 
+# mu_result<-matrix(NA,R,4)
+# 
+# for (i in 1:R) {
+# 
+#   y<-simu.ugoarma(n,phi=phi,theta=theta, alpha=alpha,sigma=sigma, tau=tau,freq=12,
+#                     link="logit")
+#   fit1<-uGoarma.fit(y)
+#   mu_result[i,]<-fit1$coeff
+# }
+# 
+# 
+# mean_values<-c(apply(mu_result,2,mean))
+# b_values<-(true_values-mean_values)/true_values*100
+# eqm_values<-c(apply(mu_result,2,var))+(true_values-mean_values)^2
+# result1<- cbind(true_values,
+#                 mean_values,
+#                 b_values,
+#                 eqm_values
+# )
+# colnames(result1)<-c("true value","mean","bias","eqm")
+# rownames(result1)<-c("alpha","phi","theta","sigma") 
+# print(round(result1,5))
 #########
 
+rm(list = ls())
 
-# Parâmetros iniciais
+
+source("simu.ugoarma.R")
+source("ugo_fit.R")
+
 phi = 0.2
 theta = 0.4
 alpha = 1
 sigma = 6
 tau = 0.5
 true_values = c(1, 0.2, 0.4, 6)
+vn = c(30,35,40,50 )#150, 300, 500
+R = 10
+z = 1.96
 
-# Tamanhos das amostras
-sample_sizes = c(70, 150, 300, 500)
-R = 100  # Número de simulações
+results = list()
+coverage = list()
 
-# Função para calcular a cobertura do intervalo de confiança de 95%
-calculate_coverage <- function(estimates, true_value, alpha = 0.05) {
-  lower_bound <- apply(estimates, 2, function(x) quantile(x, probs = alpha / 2))
-  upper_bound <- apply(estimates, 2, function(x) quantile(x, probs = 1 - alpha / 2))
-  coverage <- mean((true_value >= lower_bound) & (true_value <= upper_bound))
-  return(coverage)
-}
-
-# Matriz para armazenar resultados
-results <- list()
-
-# Loop para diferentes tamanhos de amostra
-for (n in sample_sizes) {
-  mu_result <- matrix(NA, R, 4)
+for (n in vn) {
+  mu_result = matrix(NA, R, 4)
+  ci_lower = matrix(NA, R, 4)
+  ci_upper = matrix(NA, R, 4)
   
   for (i in 1:R) {
+    #print(c("i=",i))
     y <- simu.ugoarma(n, phi = phi, theta = theta, alpha = alpha, sigma = sigma, tau = tau, freq = 12, link = "logit")
     fit1 <- uGoarma.fit(y)
     mu_result[i, ] <- fit1$coeff
+    
+    # CI 
+   
+    ci_lower[i, ] <- fit1$coeff - z * fit1$stderror
+    ci_upper[i, ] <- fit1$coeff + z * fit1$stderror
   }
   
   mean_values <- apply(mu_result, 2, mean)
   b_values <- (true_values - mean_values) / true_values * 100
   eqm_values <- apply(mu_result, 2, var) + (true_values - mean_values)^2
-  cr95_values <- sapply(1:4, function(i) calculate_coverage(mu_result[, i], true_values[i]))
   
-  result <- cbind(true_values, mean_values, b_values, eqm_values, cr95_values)
-  colnames(result) <- c("true value", "mean", "bias", "eqm", "cr95")
-  rownames(result) <- c("alpha", "phi", "theta", "sigma")
+  coverage_rate = colMeans((true_values >= ci_lower) & (true_values <= ci_upper)) * 100
   
-  results[[paste0("n=", n)]] <- result
+  result = cbind(true_values, mean_values, b_values, eqm_values, coverage_rate)
+  colnames(result) <- c("true value", "mean", "bias", "eqm", "coverage")
+  rownames(result) <- c("b1", "b2", "g1", "g2")
+  
+  results[[as.character(n)]] = result
 }
 
-# Exibindo os resultados
-for (n in sample_sizes) {
-  print(paste("Sample size:", n))
-  print(round(results[[paste0("n=", n)]], 5))
+# Impressão dos resultados
+for (n in vn) {
+  cat("\nSample size:", n, "\n")
+  print(round(results[[as.character(n)]], 5))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+fit1$zstat
+
+
+
+
 
---
+++ monte_carlo3.R
@@ -0,0 +1,88 @@
+rm(list = ls())
+
+
+source("simu.ugoarma.R")
+source("ugo_fit.R")
+
+alpha = 1
+phi = 0.2
+theta = 0.4
+sigma = 6
+tau = 0.5
+
+true_values = c(1, 0.2, 0.4, 6) # alpha, phi, theta, sigma
+vn = c(70,150, 300, 500)#150, 300, 500
+R = 20
+z = 1.96
+
+results = list()
+coverage = list()
+
+for (n in vn) {
+  #matriz de resultados
+  estim <-ICi<-ICs<- err <- matrix(NA, nrow = R, ncol = length(true_values))
+  #contadores
+  calpha<-cphi<-ctheta<-csigma<-0 #para guardar os resultados
+  i<-0
+  
+  for (i in 1:R) {
+    #print(c("i=",i))
+    y <- simu.ugoarma(n, phi = phi, theta = theta, alpha = alpha, sigma = sigma, tau = tau, freq = 12, link = "logit")
+    fit1 <- uGoarma.fit(y)
+    
+    
+    
+    estim[i,]<-fit1$model[,1]
+    err[i,]<-fit1$model[,2]
+    
+    #IC
+    ICi[i,]<-estim[i,]-(z*err[i,])
+    ICs[i,]<-estim[i,]+ (z*err[i,])
+    
+    if (ICi[i,1]<=alpha && ICs[i,1]>=alpha) 
+    {
+      calpha<-calpha+1
+    }
+    
+    if (ICi[i,2]<= phi && ICs[i,2]>=phi) 
+    {
+      cphi<-cphi+1
+    }
+    
+    if (ICi[i,3]<=theta && ICs[i,3]>=theta) 
+    {
+      ctheta<-ctheta+1
+    }
+    
+    if (ICi[i,4]<=sigma && ICs[i,4]>=sigma) 
+    {
+      csigma<-csigma+1
+    }
+    
+  }
+  
+  ### mean
+  m <- apply(estim, 2, mean)
+  ### bias
+  bias <- (true_values-m)
+  ### relative percentage bias
+  biasP <- bias/true_values *100
+  ### SD
+  erro <- apply(estim, 2, sd)
+  ### MSE
+  MSE <- apply(estim, 2, var)+bias^2
+  ### 
+  TC<-c(calpha,
+        cphi,
+        ctheta,
+        csigma)/R
+  ## final results
+  results <- rbind(m, bias, biasP, erro, MSE,TC)
+  rownames(results) <- c("Mean", "Bias","RB%", "SE", "MSE","TC")
+  colnames(results) <- c("alpha", "phi", "theta", "sigma")
+  print(c("Tamanho da Amostra:",n))
+  print(round(results,4))
+  
+}
+
+
