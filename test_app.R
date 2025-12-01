652003086e6d6a7d3139e37301de4d997fe8b897
Thiago Tavares <thiago.tlopess@gmail.com>
2025-12-01 16:07:41 -0300

    att

--- test_app.R
+++ test_app.R
@@ -11,7 +11,8 @@ library(lmtest)
 # INTEREST RATE
 dados <- readxl::read_excel("dataset.xlsx", na = "-")
 # SERIE
-Y <- ts(dados[2] / 100, frequency = 12)
+Y <- ts(dados[2] / 100, start = c(2011, 3), 
+        end = c(2025, 3), frequency = 12)
 
 m <- length(Y)
 h1 <- 6
@@ -24,17 +25,26 @@ y_train <- ts(Y[1:n], frequency = 12)
 y_test <- Y[(n + 1):m]
 
 #### REGRESSORS MATRIX ----
-t <- 1:length(y_train)
-t_hat <- (n + 1):(n + h1)
 
-# SEASONALITY
-C <- cos(2 * pi * t / 12)
-C_hat <- cos(2 * pi * t_hat / 12)
+# TREND AND SEASONAL
 
-X <- as.matrix(C)
-X_hat <- as.matrix(C_hat)
+t      <- 1:length(y_train)
+t_hat  <- (n + 1):(n + h1)
 
-nX <- dim(X)[2]
+t_sc      <- t / max(t)         
+t_hat_sc  <- t_hat / max(t)
+
+C      <- cos(2 * pi * t / 12)
+C_hat  <- cos(2 * pi * t_hat / 12)
+
+X     <- as.matrix(cbind(trend = t_sc, seasonal = C))
+X_hat <- as.matrix(cbind(trend = t_hat_sc, seasonal = C_hat))
+
+#X <- as.matrix(cbind(seasonal = C, trend = t))
+#X_hat <- as.matrix(cbind(seasonal = C_hat, trend = t_hat))
+
+
+nX <- ncol(X) #nX <- dim(X)[2]
 X0 <- rbind(X, X_hat)
 
 #### ARIMA FIT ----
@@ -53,18 +63,17 @@ qmax <- 3
 y <- y_train
 
 # FIT WITH REGRESSORS -----
-best_ugoarmax <- best_ugo_2(
-  y_train,
-  pmax = pmax, 
-  qmax = qmax,
-  nbest = 8, 
-  X = X, 
-  X_hat = X_hat
+best_ugoarmax <- best_ugo_2(y_train,
+                            pmax = pmax, qmax = qmax,
+                            nbest = 8, X = X, X_hat = X_hat
 )
 
 
 if (best_ugoarmax$q[1] == 0) {
-  fit_ugoarmax <- uGoarma.fit(y_train, ar = 1:best_ugoarmax$p[1], ma = NA, X = X, X_hat = X_hat)
+  fit_ugoarmax <- uGoarma.fit(
+    y_train, 
+    ar = 1:best_ugoarmax$p[1], 
+    ma = NA, X = X, X_hat = X_hat)
 } else {
   fit_ugoarmax <- uGoarma.fit(y_train, ar = 1:best_ugoarmax$p[1], ma = 1:best_ugoarmax$q[1], X = X, X_hat = X_hat)
 }
@@ -73,8 +82,8 @@ if (best_ugoarmax$q[1] == 0) {
 # FIT WITHOUT REGRESSORS -------
 
 best_ugoarma <- best_ugo_2(y_train,
-  pmax = pmax, qmax = qmax,
-  nbest = 8
+                           pmax = pmax, qmax = qmax,
+                           nbest = 8
 )
 
 if (best_ugoarma$q[1] == 0) {
@@ -99,18 +108,6 @@ colnames(order) <- c(
 )
 
 
-# barmax <- summary(BARFIMA.fit(y_train,
-#   p = 1, d = F, q = 0, info = T,
-#   start = list(
-#     phi = rep(0.3, 1),
-#     # theta = rep(0.3, 1),
-#     beta = coef(lm(y_train ~ X + 0)),
-#     nu = 0.01
-#   ),
-#   control = list(method = "Nelder-Mead", stopcr = 1e-2),
-#   xreg = X
-# ))
-
 
 for (i in 0:3) {
   for (j in 0:3) {
@@ -127,9 +124,10 @@ for (i in 0:3) {
       control = list(method = "Nelder-Mead", stopcr = 1e-2),
       report = F
     ))
-
-
-    barmax <- summary(BARFIMA.fit(y_train,
+    
+    
+    barmax <- summary(BARFIMA.fit(
+      y_train,
       p = i,
       d = F,
       q = j,
@@ -140,25 +138,30 @@ for (i in 0:3) {
         beta = coef(lm(y_train ~ X + 0)),
         nu = 0.01
       ),
-      control = list(method = "Nelder-Mead", stopcr = 1e-2),
+      control = list(
+        method = "Nelder-Mead", 
+        stopcr = 1e-2),
       xreg = X,
       report = F
     ))
-
-
-    karma1 <- suppressWarnings(KARFIMA.fit(y_train,
+    
+    karma1 <- suppressWarnings(KARFIMA.fit(
+      y_train,
       p = i,
       d = F,
       q = j,
       info = T,
       rho = quant,
-      control = list(method = "Nelder-Mead", stopcr = 1e-2),
+      control = list(
+        method = "Nelder-Mead", 
+        stopcr = 1e-2),
       report = F
     ))
+    
     karma <- summary(karma1)
-
-
-    karmax1 <- suppressWarnings(KARFIMA.fit(y_train,
+    
+    karmax1 <- suppressWarnings(KARFIMA.fit(
+      y_train,
       p = i,
       d = F,
       q = j,
@@ -166,13 +169,13 @@ for (i in 0:3) {
       xreg = X,
       rho = quant,
       control = list(method = "Nelder-Mead", stopcr = 1e-2),
-      report = F
+                                            report = F
     ))
     karmax <- summary(karmax1)
-
+    
     if (karma1$convergence == 1 || is.nan(karma$aic) == 1) karma$aic <- 0
     if (karmax1$convergence == 1 || is.nan(karmax$aic) == 1) karmax$aic <- 0
-
+    
     order[cont, ] <- c(
       i, j, barma$aic, karma$aic,
       barmax$aic, karmax$aic
@@ -190,40 +193,40 @@ orbarmax <- order[which(order[, 5] == min(order[, 5])), c(1:2, 5)]
 orkarmax <- order[which(order[, 6] == min(order[, 6])), c(1:2, 6)]
 
 barma <- BARFIMA.fit(y_train,
-  p = orbarma[1],
-  d = F,
-  q = orbarma[2],
-  rho = quant,
-  info = T,
-  start = list(
-    alpha = 0,
-    phi = rep(0.3, orbarma[1]),
-    if (orbarma[2] == 0){
-      NULL
-    }else{
-      theta = rep(0.3, orbarma[2]) 
-    },
-    #theta = rep(0.3, 1),
-    nu = 0.01
-  ),
-  control = list(
-    method = "Nelder-Mead",
-    stopcr = 1e-2
-  ),
-  report = F
+                     p = orbarma[1],
+                     d = F,
+                     q = orbarma[2],
+                     rho = quant,
+                     info = T,
+                     start = list(
+                       alpha = 0,
+                       phi = rep(0.3, orbarma[1]),
+                       if (orbarma[2] == 0){
+                         NULL
+                       }else{
+                         theta = rep(0.3, orbarma[2]) 
+                       },
+                       #theta = rep(0.3, 1),
+                       nu = 0.01
+                     ),
+                     control = list(
+                       method = "Nelder-Mead",
+                       stopcr = 1e-2
+                     ),
+                     report = F
 )
 
 karma <- KARFIMA.fit(y_train,
-  p = orkarma[1],
-  d = F,
-  q = orkarma[2],
-  rho = quant,
-  control = list(
-    method = "Nelder-Mead",
-    stopcr = 1e-2
-  ),
-  info = T,
-  report = F
+                     p = orkarma[1],
+                     d = F,
+                     q = orkarma[2],
+                     rho = quant,
+                     control = list(
+                       method = "Nelder-Mead",
+                       stopcr = 1e-2
+                     ),
+                     info = T,
+                     report = F
 )
 
 
@@ -236,34 +239,35 @@ start_barmax <- list(
 )
 
 
-barmax <- BARFIMA.fit(
-  y_train, 
-  p = orbarmax[1], 
-  d = F,
-  q = orbarmax[2],
-  xreg = X,
-  rho = quant,
-  start = start_barmax,
-  control = list(
-    method = "Nelder-Mead",
-    stopcr = 1e-2
-    ),
-  info = T,
-  report = F
-  )
+barmax <- BARFIMA.fit(y_train,
+                      p = orbarmax[1],
+                      d = F,
+                      q = orbarmax[2],
+                      xreg = X,
+                      rho = quant,
+                      start = start_barmax,
+                      control = list(
+                        method = "Nelder-Mead",
+                        stopcr = 1e-2
+                      ),
+                      info = T,
+                      report = F
+)
+
+
 
 karmax <- KARFIMA.fit(y_train,
-  p = orkarmax[1],
-  d = F,
-  q = orkarmax[2],
-  rho = quant,
-  control = list(
-    method = "Nelder-Mead",
-    stopcr = 1e-2
-  ),
-  xreg = X,
-  info = T,
-  report = F
+                      p = orkarmax[1],
+                      d = F,
+                      q = orkarmax[2],
+                      rho = quant,
+                      control = list(
+                        method = "Nelder-Mead",
+                        stopcr = 1e-2
+                      ),
+                      xreg = X,
+                      info = T,
+                      report = F
 )
 
 
@@ -314,6 +318,7 @@ karma_out <- KARFIMA.extract(
     nu = karma$coefficients[(orkarma[1] + 2 + orkarma[2])]
   )
 )
+
 
 barmax_out <- BARFIMA.extract(
   yt = Y, xreg = X0,
